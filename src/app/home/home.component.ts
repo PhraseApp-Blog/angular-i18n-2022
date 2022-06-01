caedebcc8070f4bd1eb6cbcda01f2e5d216dc344
Mohammad Ashour <ashour.mohammad@gmail.com>
2022-06-01 17:57:48 +0100

    Cleanup: removing ununsed styles

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -5,7 +5,6 @@ import { CabinetService } from '../cabinet.service';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css'],
 })
 export class HomeComponent implements OnInit {
   cabinets: Cabinet[] = [];
