53982e0c65e6d6456945ad9cf89dfa52424e8067
Mohammad Ashour <ashour.mohammad@gmail.com>
2022-06-01 20:35:33 +0100

    Adding production testing server and helper build-serve npm script

---
+++ serve-prod-test.js
@@ -0,0 +1,28 @@
+const path = require('path');
+const express = require('express');
+
+const port = 8080;
+const rootDir = path.join(__dirname, 'dist/azcadea');
+
+const locales = ['en-CA', 'ar'];
+const defaultLocale = 'en-CA';
+
+const server = express();
+
+server.use(express.static(rootDir));
+
+locales.forEach((locale) => {
+  server.get(`/${locale}/*`, async (req, res) => {
+    res.sendFile(
+      path.resolve(rootDir, locale, 'index.html')
+    );
+  });
+});
+
+server.get('/', (req, res) =>
+  res.redirect(`/${defaultLocale}`)
+);
+
+server.listen(port, () =>
+  console.log(`App running at port ${port}…`)
+);
